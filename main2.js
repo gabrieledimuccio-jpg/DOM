bdce9f503792021b9a32ee2f6f53d3a3be4020e4
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-03-03 17:06:23 +0100

    terminato selfwork DOM 2

--- main2.js
+++ main2.js
@@ -1,20 +1,19 @@
+let newarticle = document.querySelector('#newarticle');
 
- let newarticle = document.querySelector('#newarticle');
-
- newarticle.addEventListener('click', () => { 
-     let title = document.querySelector('#title').value;
-     let paragrafo = document.querySelector('#paragrafo').value;
-     if (title == "" || paragrafo == "") {
+newarticle.addEventListener('click', () => { 
+    let title = document.querySelector('#title').value;
+    let paragrafo = document.querySelector('#paragrafo').value;
+    if (title == "" || paragrafo == "") {
         alert("Attenzione: devono essere inseriti sia un titolo che un paragrafo!")
-     } else {
-         let date = new Date();
-    let formatDate = date.toLocaleDateString()
-    let container = document.querySelector('.container')
-    let Article = document.createElement('p');
-    Article.innerHTML = `<h1>${title}</h1> <p>${paragrafo}</p> <p> Pubblicato in data: ${formatDate}</p>`;
-    container.appendChild(Article);
-
-     }
+    } else {
+        let date = new Date();
+        let formatDate = date.toLocaleDateString()
+        let container = document.querySelector('.container')
+        let Article = document.createElement('p');
+        Article.innerHTML = `<h1>${title}</h1> <p>${paragrafo}</p> <p> Pubblicato in data: ${formatDate}</p>`;
+        container.appendChild(Article);
+        
+    }
     document.querySelector('#title').value = "";
     document.querySelector('#paragrafo').value = "";
- });
+});
